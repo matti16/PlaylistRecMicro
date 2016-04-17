0cb95138aff2edb1a6ede31c5f8217e3a4c63025
matti16 <bmattia92@gmail.com>
2016-04-17 17:26:54 +0000

    memory overhead

--- playlistrec-master/spark-scripts/bullet.py
+++ playlistrec-master/spark-scripts/bullet.py
@@ -1,14 +1,21 @@
 from pushbullet import Pushbullet
 apiKey = 'o.jsMGG789aqsV47OChBCGlPIJjQGli3Qz'
-
+apiKeyRob = "o.uKvCG8jgepkDdoQBoUFzVJ84zCgyQ0VU"
 
 pb = Pushbullet(apiKey)
+pbRob = Pushbullet(apiKeyRob)
 
 xperia = pb.get_device('Xperia M2')
+oneplus = pbRob.get_device('OnePlus One')
 
 def sendNotificationToMattia(title,body):
     push = xperia.push_note(title, body)
+       
 
+def sendNotificationToRoberto(title,body):
+    push = oneplus.push_note(title, body)
+
+    
 sendNotificationToMattia("Ciao Mattia","Sono pronto!")
 
 
